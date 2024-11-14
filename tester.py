9cf8f985495a593c8008ab985794958161052b57
ImHarim <jjh1234y@gachon.ac.kr>
2024-11-15 00:39:12 +0900

    done

--- tester.py
+++ tester.py
@@ -1,12 +1,10 @@
-import __pycache__ as utils
+import geo.utils as utils
+import math
 
-a, b = 3,4
+a, b = 3, 4
 c = (a**2 + b**2)**0.5
 print('c =', c)
 
 r = 10
-area = r**2*3.14
+area = r**2 * math.pi
 print('area =', area)
-
-
-
