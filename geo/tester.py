be9cab46cffa2271057ac7107617db4b9bc85be9
ImHarim <jjh1234y@gachon.ac.kr>
2024-11-15 00:06:32 +0900

    again

---
+++ geo/tester.py
@@ -0,0 +1,12 @@
+import __pycache__ as utils
+
+a, b = 3,4
+c = (a**2 + b**2)**0.5
+print('c =', c)
+
+r = 10
+area = r**2*3.14
+print('area =', area)
+
+
+
---
+++ geo/utils.py
@@ -0,0 +1,13 @@
+import math
+
+def pythagoras(a,b):
+    c = math.sqrt(a**2 + b**2)
+    return c
+
+def circle(r):
+    area = math.py * r**2
+    return area
+
+
+
+
